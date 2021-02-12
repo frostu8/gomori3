842f3d2184591ecb18bde509568c3823f5d17170
Dante <frostu8@protonmail.com>
2021-02-12 17:19:22 -0500

    skeleton modloader

---
+++ gomori/gomori.js
@@ -0,0 +1,7 @@
+(() => {
+    try {
+        // load mod loader (how)
+    } catch (err) {
+        alert(`GOMORI encountered a critical error: ${err.stack}`);
+    }
+})();
---
+++ gomori/lib/ModLoader.js
@@ -0,0 +1,7 @@
+/**
+ * The Gomori Mod Loader, the supervisor of all modding activities.
+ */
+class ModLoader {
+}
+
+module.exports = ModLoader;
