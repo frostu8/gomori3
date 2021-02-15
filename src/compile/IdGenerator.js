3e5321563c08a82ad4a0c3005c40ff8425197f66
Dante <frostu8@protonmail.com>
2021-02-14 22:43:47 -0500

    id gen logic

---
+++ src/compile/IdGenerator.js
@@ -0,0 +1,45 @@
+/**
+ * Takes a list of ids, and allows the efficient generation of new, 
+ * unallocated ids
+ */
+class IdGenerator {
+    constructor(allocated) {
+        // For some reason, we have to pass a sorting callback to Array.sort(),
+        // because you don't deserve nice things.
+        this._allocated = this.allocated.sort((a, b) => a - b);
+
+        // Initial value of last_id is based off of the first id of each 
+        // database model in RPGMV minus one. The first id of database models
+        // is always 1, so this value starts as zero.
+        this._last_id = 0;
+    }
+
+    /**
+     * Returns a new id from the pool.
+     *
+     * This depends on this.allocated to be sorted properly.
+     */
+    new_id() {
+        // Assuming that the list is sorted (which it should be), we can take
+        // the next id from the list and compare it with our next id
+        while (this._allocated.length > 0) {
+            // set last id to test
+            this._last_id++;
+
+            // get next id
+            let next_id = this._allocated[0];
+            this._allocated = this.allocated.slice(1);
+
+            // doesn't hurt to test
+            if (next_id < this._last_id) throw new Error('improperly sorted id list!');
+
+            if (next_id > this._last_id) {
+                // this id (should) is not in the list!
+                return this._last_id;
+            }
+        }
+
+        // if we run out of allocated values, then just start returning new ids
+        return this._last_id++;
+    }
+}
