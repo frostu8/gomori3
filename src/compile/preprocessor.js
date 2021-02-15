017fa916e970cde7653212a1be6460006abf7219
Dante <frostu8@protonmail.com>
2021-02-15 12:00:52 -0500

    added tags preprocessor

---
+++ src/compile/preprocessor.js
@@ -0,0 +1,55 @@
+const DIRECTIVE_START = '<!';
+const DIRECTIVE_END = '>';
+
+const DIRECTIVE_DIVIDER = ':';
+
+/**
+ * Parse preprocessor directives.
+ *
+ * The parser reads each line, and if a preprocessor directive is found 
+ * (`<! ... >`), it includes it in the resulting object.
+ *
+ * @param {string} str - A note string to parse.
+ *
+ * @result {Object.<string, string>} - The resulting parsed object.
+ */
+function parse(str) {
+    let result = {};
+
+    let next = str.indexOf(DIRECTIVE_START);
+
+    while (next >= 0) {
+        // find end
+        let end = str.indexOf(DIRECTIVE_END, next);
+
+        if (end >= 0) {
+            // substring beginning and end
+            let directive = str.slice(next + DIRECTIVE_START.length, end) .trim();
+
+            // find divider
+            let divIdx = directive.indexOf(DIRECTIVE_DIVIDER);
+
+            if (divIdx >= 0) {
+                // a divider, get the value.
+                let key = directive.slice(0, divIdx).trim();
+                let value = directive.slice(divIdx + DIRECTIVE_DIVIDER.length).trim();
+
+                result[key] = value;
+            } else {
+                // no divider, so treat this like a raised flag.
+                result[directive] = true;
+            }
+
+            // set next iteration up
+            next = str.indexOf(DIRECTIVE_START, end + DIRECTIVE_END.length);
+        } else {
+            // unterminated tag! for now we can just ignore it, but in the
+            // future this would be nice to say.
+            break;
+        }
+    }
+
+    return result;
+}
+
+export { parse };
